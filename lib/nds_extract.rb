1e99fdbf111bb4f2eaa3e12b8eb14027547d3576
John Tom Jr <johntjr.22@gmail.com>
2020-03-10 00:20:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,7 +38,7 @@ def movies_with_director_key(name, movies_collection)
   i = 0 
   
   while i < movies_collection.length do
-  new_data = movies_collection
+  new_data = movies_collection[index] 
   
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
